32fd753d1c451988dd4fe1462923e0f04d9d8a8d
notmescooby-ui <not.mescooby@gmail.com>
2026-07-23 20:32:31 +0530

    build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,3 +43,8 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.16.0"))
+    implementation("com.google.firebase:firebase-analytics")
+}
