ffdbe0191a2cb83c3a40d80ea594716cf837d436
notmescooby-ui <not.mescooby@gmail.com>
2026-08-08 19:24:04 +0530

    fix: initialize Firebase App Check

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:flutter/foundation.dart';
 import 'firebase_options.dart';
 
 import 'providers/app_provider.dart';
@@ -18,6 +20,10 @@ void main() async {
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: kDebugMode ? AndroidProvider.debug : AndroidProvider.playIntegrity,
   );
 
   try {
